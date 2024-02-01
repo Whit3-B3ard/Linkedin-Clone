73a7e519af4fa438262fd990f83acf90e9ec9b58
KostasBzn <kostasbouzianis@gmail.com>
2024-02-01 10:27:09 +0100

    merged

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,5 @@
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -6,13 +8,18 @@ import SignUp from "./pages/SignUp";
 import UserContextProvider from "./context/userContext";
 import PostContextProvider from "./context/postContext";
 import Home from "./pages/Home";
+
 function App() {
   return (
     <>
       <BrowserRouter>
         <UserContextProvider>
           <PostContextProvider>
+            <div className="mb-14">
+              <Navbar />
+            </div>
             <Routes>
+              <Route path="/home1" element={<HomePage />} />
               <Route path="/home" element={<Home />} />
               <Route path="/signin" element={<SignIn />} />
               <Route path="/" element={<SignUp />} />
@@ -23,5 +30,4 @@ function App() {
     </>
   );
 }
-
 export default App;
